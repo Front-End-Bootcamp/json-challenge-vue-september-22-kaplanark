d5fb4b41dde4caea9475f730414605790baf276a
kaplanark <kaplanarkan@gmail.com>
2022-09-11 22:14:41 +0300

    mission suscces

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,7 +2,7 @@
 import data from "./../json/data.json";
 
 // grup ismine göre datayı filtreliyoruz ve reorganize fonksiyonuna gönderiyoruz eğer böyle bir grup yoksa bilgi veriyoruz
-const getGroups = (data, groupName) => {
+const getGroup = (data, groupName) => {
 	const groupMembers = data.filter((item) => item.group === groupName); // grup ismine göre datayı filtreliyoruz
 	if (groupMembers.length > 0) {
 		return reOrganize(groupMembers, groupName); // reorganize fonksiyonuna gönderiyoruz
@@ -16,9 +16,9 @@ const reOrganize = (groupMembers, groupName) => {
 	const newData = {
 		group: groupName, // grup ismini atıyoruz
 		assistant: groupMembers.find((member) => member.type === 'assistant').name, // asistanı buluyoruz
-		students: groupMembers.map((member) => member.type != 'assistant' ? member.name : null).filter((item) => item != null) 
+		students: groupMembers.map((member) => member.type != 'assistant' ? member.name : null).filter((name) => name != null)
 		// burda asistanı hariç tuttmak için type kontrolü yapıyoruz ve asistan adına null atıyoruz ardından null olanları filtreliyoruz ve kalanları students arrayine atıyoruz
 	}
 	console.log(newData); // yeni datayı yazdırıyoruz
 }
-getGroups(data, 'SteelBlue'); // data ile SteelBlue grup ismini getGroups fonksiyonuna gönderiyoruz
+getGroup(data, 'SteelBlue'); // data ile SteelBlue grup ismini getGroups fonksiyonuna gönderiyoruz
